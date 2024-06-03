7f3d9139de0c508285b385d83ef536b62b0adfec
llite22 <llite2@yandex.ru>
2024-06-03 12:49:23 +0300

    fix bg-color page

--- src/widgets/Page/ui/Page/Page.tsx
+++ src/widgets/Page/ui/Page/Page.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const Page = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="flex-grow h-custom py-5 px-5 pl-4 pr-4 overflow-y-auto flex-shrink">
+    <main className="flex-grow h-custom py-5 px-5 pl-4 pr-4 overflow-y-auto flex-shrink bg-gray-100 dark:bg-black">
       {children}
     </main>
   );
